a5bd7e48dd888f539a90903fa53980566dd56962
Fran <franco.camilletti.09@gmail.com>
2022-06-15 16:17:56 -0300

    Some practices on Storyboard, User interactions, AutoLayout, Constraints and Stacks

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    //Dices Views
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    //Dices Images Array
+    let diceImages: [UIImage] = [UIImage(named: "DiceOne")!,  UIImage(named: "DiceTwo")!, UIImage(named: "DiceThree")!, UIImage(named: "DiceFour")!, UIImage(named: "DiceFive")!, UIImage(named: "DiceSix")!,]
+    
+    //Roll Dices Button
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageView1.image = diceImages.randomElement()
+        diceImageView2.image = diceImages.randomElement()
     }
-
-
+    
 }
 
